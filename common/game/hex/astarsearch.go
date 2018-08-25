6c16836e8ad1e1cf607541abcf57e08d9f8870e0
karin <kf5121@student.uni-lj.si>
2018-08-25 15:16:07 +0200

    Revert optimization of hex.searchState

--- common/game/hex/astarsearch.go
+++ common/game/hex/astarsearch.go
@@ -27,21 +27,16 @@ var virtualConnections = [6][]int{
 	[]int{-2, 1},
 }
 
-// Assumption: c and gameState are the same in all searchStates that appear in
-// one run of A* search.
-var currentGameState *State // State in a game where solution is searched for
-var currentColor Color      // The color that a solution is searched for
-
 type searchState struct {
 	x, y            int          // Current position; x = -1 means a field left of the grid, y = -1 means a field above the grid
+	c               Color        // The color that a solution is searched for
+	gameState       *State       // State in a game where solution is searched for
 	prevSearchState *searchState // The preceeding search state
 }
 
 // GetInitialState returns inital state of the search
 func GetInitialState(gameState *State) searchState {
-	currentGameState = gameState
-	currentColor = gameState.lastPlayer
-	return searchState{-1, -1, nil}
+	return searchState{-1, -1, gameState.lastPlayer, gameState, nil}
 }
 
 // GetClean returns an array that is unique for each searchState.
@@ -51,8 +46,8 @@ func (s searchState) GetClean() interface{} {
 }
 
 func (s searchState) IsGoalState() (bool, interface{}) {
-	size := int(currentGameState.GetSize())
-	isGoal := (currentColor == Red && s.y >= size-1) || (currentColor == Blue && s.x >= size-1)
+	size := int(s.gameState.GetSize())
+	isGoal := (s.c == Red && s.y >= size-1) || (s.c == Blue && s.x >= size-1)
 	if isGoal {
 		return true, s.GetWinPath()
 	}
@@ -62,13 +57,13 @@ func (s searchState) IsGoalState() (bool, interface{}) {
 // GetEstimateToReachGoal returns number of cells between current state and
 // final row/column
 func (s searchState) GetEstimateToReachGoal() int {
-	switch currentColor {
+	switch s.c {
 	case Red:
-		return currentGameState.GetSize() - 1 - s.y
+		return s.gameState.GetSize() - 1 - s.y
 	case Blue:
-		return currentGameState.GetSize() - 1 - s.x
+		return s.gameState.GetSize() - 1 - s.x
 	}
-	panic(fmt.Sprintf("Unknown color %d!", currentColor))
+	panic(fmt.Sprintf("Unknown color %d!", s.c))
 }
 
 // GetSuccessorStates returns all possible successors of the searchState s.
@@ -77,18 +72,18 @@ func (s searchState) GetEstimateToReachGoal() int {
 func (s searchState) GetSuccessorStates(veryEnd bool) []astarsearch.State {
 	successors := make([]astarsearch.State, 0)
 
-	if (s.x == -1 && currentColor == Blue) || (s.y == -1 && currentColor == Red) {
+	if (s.x == -1 && s.c == Blue) || (s.y == -1 && s.c == Red) {
 		// Beginning of the search
 		a, b := 0, 0
 		xp, yp := &a, &b
-		if currentColor == Blue {
+		if s.c == Blue {
 			xp, yp = yp, xp
 		}
 
 		// Add cells in the first row/column (directly connected to the edge)
-		for ; a < int(currentGameState.size); a++ {
-			if currentGameState.getColorOn(byte(*xp), byte(*yp)) == currentColor {
-				successors = append(successors, searchState{*xp, *yp, &s})
+		for ; a < int(s.gameState.size); a++ {
+			if s.gameState.getColorOn(byte(*xp), byte(*yp)) == s.c {
+				successors = append(successors, searchState{*xp, *yp, s.c, s.gameState, &s})
 			}
 		}
 
@@ -97,14 +92,14 @@ func (s searchState) GetSuccessorStates(veryEnd bool) []astarsearch.State {
 			// connected to the player's first edge)
 			b = 1
 			xDiffFirst, yDiffFirst, xDiffSecond, yDiffSecond := 0, -1, 1, -1
-			if currentColor == Blue {
+			if s.c == Blue {
 				xDiffFirst, yDiffFirst, xDiffSecond, yDiffSecond = yDiffFirst, xDiffFirst, yDiffSecond, xDiffSecond
 			}
-			for a = 0; a < int(currentGameState.size)-1; a++ {
-				if currentGameState.getColorOn(byte(*xp), byte(*yp)) == currentColor &&
-					currentGameState.getColorOn(byte(*xp+xDiffFirst), byte(*yp+yDiffFirst)) == None &&
-					currentGameState.getColorOn(byte(*xp+xDiffSecond), byte(*yp+yDiffSecond)) == None {
-					successors = append(successors, searchState{*xp, *yp, &s})
+			for a = 0; a < int(s.gameState.size)-1; a++ {
+				if s.gameState.getColorOn(byte(*xp), byte(*yp)) == s.c &&
+					s.gameState.getColorOn(byte(*xp+xDiffFirst), byte(*yp+yDiffFirst)) == None &&
+					s.gameState.getColorOn(byte(*xp+xDiffSecond), byte(*yp+yDiffSecond)) == None {
+					successors = append(successors, searchState{*xp, *yp, s.c, s.gameState, &s})
 				}
 			}
 		}
@@ -112,8 +107,8 @@ func (s searchState) GetSuccessorStates(veryEnd bool) []astarsearch.State {
 		// Add direct neighbours
 		for _, n := range neighbours {
 			x, y := s.x+n[0], s.y+n[1]
-			if currentGameState.IsCellValid(x, y) && currentGameState.getColorOn(byte(x), byte(y)) == currentColor {
-				successors = append(successors, searchState{x, y, &s})
+			if s.gameState.IsCellValid(x, y) && s.gameState.getColorOn(byte(x), byte(y)) == s.c {
+				successors = append(successors, searchState{x, y, s.c, s.gameState, &s})
 			}
 		}
 		if !veryEnd {
@@ -137,17 +132,17 @@ func (s searchState) GetSuccessorStates(veryEnd bool) []astarsearch.State {
 				// Change relative coordinates to absolute coordinates
 				x1, x2, y1, y2 = s.x+x1, s.x+x2, s.y+y1, s.y+y2
 
-				if currentGameState.IsCellValid(x1, y1) && currentGameState.getColorOn(byte(x1), byte(y1)) != None ||
-					currentGameState.IsCellValid(x2, y2) && currentGameState.getColorOn(byte(x2), byte(y2)) != None {
+				if s.gameState.IsCellValid(x1, y1) && s.gameState.getColorOn(byte(x1), byte(y1)) != None ||
+					s.gameState.IsCellValid(x2, y2) && s.gameState.getColorOn(byte(x2), byte(y2)) != None {
 					continue // At least one of these cells is not empty
 				}
 				// Both cells in between are empty
 
 				x, y := s.x+v[0], s.y+v[1]
 
-				if currentGameState.IsEndingCell(x, y, currentColor) ||
-					currentGameState.IsCellValid(x, y) && currentGameState.getColorOn(byte(x), byte(y)) == currentColor {
-					successors = append(successors, searchState{x, y, &s})
+				if s.gameState.IsEndingCell(x, y, s.c) ||
+					s.gameState.IsCellValid(x, y) && s.gameState.getColorOn(byte(x), byte(y)) == s.c {
+					successors = append(successors, searchState{x, y, s.c, s.gameState, &s})
 				}
 			}
 		}
@@ -156,13 +151,13 @@ func (s searchState) GetSuccessorStates(veryEnd bool) []astarsearch.State {
 }
 
 func (s searchState) String() string {
-	return fmt.Sprintf("x: %d, y: %d, c: %s, state:\n%s", s.x, s.y, currentColor, currentGameState.String())
+	return fmt.Sprintf("x: %d, y: %d, c: %s, state:\n%s", s.x, s.y, s.c, s.gameState.String())
 }
 
 // GetWinPath returns coordinates of the cells in the winning (virtual)
 // connection.
 func (s *searchState) GetWinPath() [][2]int {
-	path := make([][2]int, 0, currentGameState.GetSize())
+	path := make([][2]int, 0, s.gameState.GetSize())
 
 	cs := s
 	for cs != nil {
