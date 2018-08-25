2c9d784664c2682b9f800fb52bea232ec3390cb4
karin <kf5121@student.uni-lj.si>
2018-08-25 12:49:09 +0200

    Time limited iterative deepening (AB) finished

--- 3-ab/ab/ab.go
+++ 3-ab/ab/ab.go
@@ -1,8 +1,9 @@
 package ab
 
 import (
+	"context"
 	"fmt"
-	"log"
+	"sync"
 	"time"
 
 	"github.com/RdecKa/bachleor-thesis/common/game/hex"
@@ -18,43 +19,85 @@ func AlphaBeta(state *hex.State, patFileName string) (*hex.Action, *tree.Tree) {
 	gridChan, stopChan, resultChan := hex.CreatePatChecker(patFileName)
 	defer func() { stopChan <- struct{}{} }()
 
-	timer := time.NewTimer(3 * time.Second)
-	timeOut := false
+	// Make channels for communication with alphaBetaWorker
+	actionChan := make(chan *hex.Action, 1)
+	nodeChan := make(chan *tree.Node, 1)
+	defer close(actionChan)
+	defer close(nodeChan)
 
-	var a *hex.Action
+	var selectedAction *hex.Action
 	var rootNode *tree.Node
-	var err error
 
-	for depth := 2; !timeOut; depth += 2 {
+	timeout := 1 * time.Second
+	ctx, cancel := context.WithTimeout(context.Background(), timeout)
+	var wg sync.WaitGroup
+
+	boardSize := state.GetSize()
+	for depth := 2; depth < boardSize*boardSize; depth += 2 {
+		// Start a worker
+		wg.Add(1)
+		go alphaBetaWorker(ctx, &wg, state, depth, gridChan, resultChan, actionChan, nodeChan)
+
+		// Wait for worker to send results or for timeout
 		select {
-		case <-timer.C:
-			timeOut = true
-		default:
-			fmt.Printf("Starting AB on depth %d\n", depth)
-			transpositionTable := make(map[string]float64)
-			_, a, rootNode, err = alphaBeta(depth, state, nil, -abInit, abInit, gridChan, resultChan, transpositionTable)
-
-			fmt.Printf("Selected action: %v\n", a)
-
-			if err != nil {
-				log.Println(err)
+		case <-ctx.Done(): // Timeout
+			goto endForLoop
+		case selectedAction = <-actionChan: // Results received
+			rootNode = <-nodeChan
+			if selectedAction == nil {
+				// If an action was not found in a shallower search, it will not
+				// be found in a deeper search
+				goto endForLoop
 			}
 		}
 	}
 
-	if a == nil {
+endForLoop:
+	// Cancel the last AB search
+	cancel()
+	// Wait till the goroutine with the last AB search is completely cancelled
+	wg.Wait()
+
+	if selectedAction == nil {
 		// "Random" - TODO
 		fmt.Println("Choosing 'randomly'")
 		possibleActions := state.GetPossibleActions()
 		return possibleActions[0].(*hex.Action), nil
 	}
 
-	return a, tree.NewTree(rootNode)
+	return selectedAction, tree.NewTree(rootNode)
 }
 
-func alphaBeta(depth int, state *hex.State, lastAction *hex.Action,
+func alphaBetaWorker(ctx context.Context, wg *sync.WaitGroup, state *hex.State, depth int,
+	gridChan chan []uint32, resultChan chan [2][]int, actionChan chan *hex.Action, nodeChan chan *tree.Node) {
+
+	defer wg.Done() // Signal that the search is finished (normally or because it was cancelled)
+
+	fmt.Printf("Starting AB on depth %d\n", depth)
+
+	transpositionTable := make(map[string]float64)
+	_, a, rootNode, err := alphaBeta(ctx, depth, state, nil, -abInit, abInit, gridChan, resultChan, transpositionTable)
+
+	if err != nil {
+		fmt.Println(err)
+	}
+	fmt.Printf("Selected action: %v\n", a)
+
+	// Send results
+	actionChan <- a
+	nodeChan <- rootNode
+}
+
+func alphaBeta(ctx context.Context, depth int, state *hex.State, lastAction *hex.Action,
 	alpha, beta float64, gridChan chan []uint32, resultChan chan [2][]int,
 	transpositionTable map[string]float64) (float64, *hex.Action, *tree.Node, error) {
+
+	// End recursion on timeout
+	select {
+	case <-ctx.Done():
+		return 0, nil, nil, ctx.Err()
+	default:
+	}
 
 	if val, ok := transpositionTable[state.GetMapKey()]; ok {
 		// Current state was already investigated
@@ -84,8 +127,15 @@ func alphaBeta(depth int, state *hex.State, lastAction *hex.Action,
 	nodeChildren := make([]*tree.Node, 0, len(possibleActions))
 	comment := ""
 	for _, a := range possibleActions {
+		// End recursion on timeout
+		select {
+		case <-ctx.Done():
+			return 0, nil, nil, ctx.Err()
+		default:
+		}
+
 		successor := state.GetSuccessorState(a).(hex.State)
-		value, _, childNode, err := alphaBeta(depth-1, &successor, a.(*hex.Action), -beta, -alpha, gridChan, resultChan, transpositionTable)
+		value, _, childNode, err := alphaBeta(ctx, depth-1, &successor, a.(*hex.Action), -beta, -alpha, gridChan, resultChan, transpositionTable)
 		if err != nil {
 			return 0, nil, nil, err
 		}
