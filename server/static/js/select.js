a52bc57e28082b6d69d414f2afc226771964b7da
karin <kf5121@student.uni-lj.si>
2019-01-04 12:43:15 +0100

    Frontend design

--- server/static/js/select.js
+++ server/static/js/select.js
@@ -69,25 +69,25 @@ Vue.component("select-player", {
 				<label :for="'human-' + color">Human</label>
 				<br>
 				<input type="radio" :id="'rand-' + color" :name="color" value="rand" v-model="player" @change="selectionChange" />
-				<label :for="'rand-' + color">Random</label>
+				<label :for="'rand-' + color">RAND</label>
 				<br>
 				<input type="radio" :id="'mcts-'  + color" :name="color" value="mcts"  v-model="player" @change="selectionChange" />
-				<label :for="'mcts-'  + color">Computer (MCTS)</label>
+				<label :for="'mcts-'  + color">MCTS</label>
 				<input type="number" min="1" :id="'time-mcts-' + color" v-model="time.mcts" @change="selectionChange">
 				<label :for="'time-mcts-' + color">seconds</label>
 				<br>
 				<input type="radio" :id="'abDT-'  + color" :name="color" value="abDT"  v-model="player" @change="selectionChange" />
-				<label :for="'abDT-'  + color">Computer (AB-DL)</label>
+				<label :for="'abDT-'  + color">ABDL</label>
 				<input type="number" min="1" :id="'time-abDT-' + color" v-model="time.abDT" @change="selectionChange">
 				<label :for="'time-abDT-' + color">seconds</label>
 				<br>
 				<input type="radio" :id="'abLR-'  + color" :name="color" value="abLR"  v-model="player" @change="selectionChange" />
-				<label :for="'abLR-'  + color">Computer (AB-LR)</label>
+				<label :for="'abLR-'  + color">ABLR</label>
 				<input type="number" min="1" :id="'time-abLR-' + color" v-model="time.abLR" @change="selectionChange">
 				<label :for="'time-abLR-' + color">seconds</label>
 				<br>
 				<input type="radio" :id="'hybrid-'  + color" :name="color" value="hybrid"  v-model="player" @change="selectionChange" />
-				<label :for="'hybrid-'  + color">Computer (hybrid)</label>
+				<label :for="'hybrid-'  + color">HYBR</label>
 				<input type="number" min="1" :id="'time-hybrid-' + color" v-model="time.hybrid" @change="selectionChange">
 				<label :for="'time-hybrid-' + color">seconds</label>
 			</div>
